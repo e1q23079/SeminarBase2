1b3ab0df8844451c3215e800d37154f42e704eff
e1q23079 <156163994+e1q23079@users.noreply.github.com>
2026-02-10 19:25:17 +0900

    管理サイトのタイトルを変更するコメントを追加

--- app/seminar_base2/admin.py
+++ app/seminar_base2/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-# Customize the admin site headers and titles
+# 管理サイトのタイトルを変更
 admin.site.site_header = "SeminarBase2 管理者サイト"
 admin.site.site_title = "SeminarBase2"
 admin.site.index_title = "管理画面"
