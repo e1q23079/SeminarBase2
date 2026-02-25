2fb7f9b167e1e2258b1a7f69678b4eaf9319f263
e1q23079 <156163994+e1q23079@users.noreply.github.com>
2026-02-25 23:09:22 +0900

    テスト用のGitHub Actionsワークフローを追加

--- app/seminar_base2/tests.py
+++ app/seminar_base2/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class IndexViewTests(TestCase):
+    def test_index_view(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
